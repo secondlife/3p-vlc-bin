da9c363e0aa23da3892d1c3f6cba02b6cfee2ddf
callum_linden <none@none>
2018-01-04 19:47:49 -0800

    Code drop for LibVLC 2.2.8 for macOS
    --HG--
    branch : vendor

--- vlc-darwin64/include/vlc/libvlc_version.h
+++ vlc-darwin64/include/vlc/libvlc_version.h
@@ -39,7 +39,7 @@
 # define LIBVLC_VERSION_MINOR    (2)
 
 /** LibVLC revision */
-# define LIBVLC_VERSION_REVISION (4)
+# define LIBVLC_VERSION_REVISION (8)
 
 # define LIBVLC_VERSION_EXTRA    (0)
 
